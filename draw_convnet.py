16df456229291e0f4952583c7570096343da4435
Hugo Feng <hugo.fxy@gmail.com>
2016-05-06 22:29:42 +0800

    tweak the original code for displaying feature maps and kernels which may not be square shaped

--- draw_convnet.py
+++ draw_convnet.py
@@ -39,7 +39,7 @@ from matplotlib.patches import Rectangle
 from matplotlib.collections import PatchCollection
 
 
-NumConvMax = 8
+NumConvMax = 12
 NumFcMax = 20
 White = 1.
 Light = 0.7
@@ -48,16 +48,16 @@ Dark = 0.3
 Black = 0.
 
 
-def add_layer(patches, colors, size=24, num=5,
+def add_layer(patches, colors, size=(24, 24), num=5,
               top_left=[0, 0],
               loc_diff=[3, -3],
               ):
     # add a rectangle
     top_left = np.array(top_left)
     loc_diff = np.array(loc_diff)
-    loc_start = top_left - np.array([0, size])
+    loc_start = top_left - np.array([0, size[-1]]) # lower left xy of a certain rectangle
     for ind in range(num):
-        patches.append(Rectangle(loc_start + ind * loc_diff, size, size))
+        patches.append(Rectangle(loc_start + ind * loc_diff, size[-2], size[-1]))
         if ind % 2:
             colors.append(Medium)
         else:
@@ -69,30 +69,30 @@ def add_mapping(patches, colors, start_ratio, patch_size, ind_bgn,
 
     start_loc = top_left_list[ind_bgn] \
         + (num_show_list[ind_bgn] - 1) * np.array(loc_diff_list[ind_bgn]) \
-        + np.array([start_ratio[0] * size_list[ind_bgn],
-                    -start_ratio[1] * size_list[ind_bgn]])
+        + np.array([start_ratio[-2] * size_list[ind_bgn][-2],
+                    -start_ratio[-1] * size_list[ind_bgn][-1]])
 
     end_loc = top_left_list[ind_bgn + 1] \
         + (num_show_list[ind_bgn + 1] - 1) \
         * np.array(loc_diff_list[ind_bgn + 1]) \
-        + np.array([(start_ratio[0] + .5 * patch_size / size_list[ind_bgn]) *
-                    size_list[ind_bgn + 1],
-                    -(start_ratio[1] - .5 * patch_size / size_list[ind_bgn]) *
-                    size_list[ind_bgn + 1]])
+        + np.array([(start_ratio[0] + .5 * patch_size[-2] / size_list[ind_bgn + 1][-2]) *
+                    size_list[ind_bgn + 1][-2] - 0.5,
+                    -(start_ratio[1] - .5 * patch_size[-1] / size_list[ind_bgn + 1][-1]) *
+                    size_list[ind_bgn + 1][-1]])
 
-    patches.append(Rectangle(start_loc, patch_size, patch_size))
+    patches.append(Rectangle(start_loc, patch_size[-2], patch_size[-1]))
     colors.append(Dark)
     patches.append(Line2D([start_loc[0], end_loc[0]],
                           [start_loc[1], end_loc[1]]))
     colors.append(Black)
-    patches.append(Line2D([start_loc[0] + patch_size, end_loc[0]],
+    patches.append(Line2D([start_loc[0] + patch_size[-2], end_loc[0]],
                           [start_loc[1], end_loc[1]]))
     colors.append(Black)
     patches.append(Line2D([start_loc[0], end_loc[0]],
-                          [start_loc[1] + patch_size, end_loc[1]]))
+                          [start_loc[1] + patch_size[-1], end_loc[1]]))
     colors.append(Black)
-    patches.append(Line2D([start_loc[0] + patch_size, end_loc[0]],
-                          [start_loc[1] + patch_size, end_loc[1]]))
+    patches.append(Line2D([start_loc[0] + patch_size[-2], end_loc[0]],
+                          [start_loc[1] + patch_size[-1], end_loc[1]]))
     colors.append(Black)
 
 
@@ -104,7 +104,7 @@ def label(xy, text, xy_off=[0, 4]):
 if __name__ == '__main__':
 
     fc_unit_size = 2
-    layer_width = 40
+    layer_width_min = 40
 
     patches = []
     colors = []
@@ -113,12 +113,32 @@ if __name__ == '__main__':
 
 
     ############################
-    # conv layers
-    size_list = [32, 18, 10, 6, 4]
-    num_list = [3, 32, 32, 48, 48]
-    x_diff_list = [0, layer_width, layer_width, layer_width, layer_width]
+    # feature maps
+    # settings: The 'kernel' or 'map' key may have one or two pairs of (w, h), such as the first 'kernel' (1, 3, 3, 9).
+    #           The first pair (1, 3) is the actual network param used for text display,
+    #           the second pair (3, 9) is the scaled pixel size only for plotting on large feature maps.
+    #           Same thing applies to 'map' size, where size used for plotting may be smaller than the actual size.
+    input_setting = {'map': (224, 224, 56, 56), 'map_num': 3}
+    network_settings = [{'type': 'Convolution1_v', 'kernel': (1, 3, 3, 9), 'map': (224, 224, 56, 56), 'map_num': 5},
+                        {'type': 'Convolution1_h', 'kernel': (3, 1, 9, 3), 'map': (224, 224, 56, 56), 'map_num': 16},
+                        {'type': 'Max-pooling', 'kernel': (2, 2, 6, 6), 'map': (112, 112, 40, 40), 'map_num': 16},
+                        {'type': 'Convolution2', 'kernel': (3, 1, 9, 3), 'map': (112, 112, 40, 40), 'map_num': 32},
+                        {'type': 'Max-pooling', 'kernel': (2, 2, 6, 6), 'map': (56, 56, 32, 32), 'map_num': 32},
+                        {'type': 'Convolution3', 'kernel': (3, 1, 9, 3), 'map': (56, 56, 32, 32), 'map_num': 64},
+                        {'type': 'Max-pooling', 'kernel': (2, 2, 6, 6), 'map': (28, 28, 20, 20), 'map_num': 64},
+                        {'type': 'Convolution4', 'kernel': (3, 1, 9, 3), 'map': (28, 28, 20, 20), 'map_num': 128},
+                        {'type': 'Max-pooling', 'kernel': (2, 2, 6, 6), 'map': (14, 14), 'map_num': 128},
+                        {'type': 'Convolution5', 'kernel': (3, 1), 'map': (14, 14), 'map_num': 256},
+                        {'type': 'Max-pooling', 'kernel': (2, 2), 'map': (7, 7), 'map_num': 256}]
+    size_list = [input_setting['map']] + [setting['map'] for setting in network_settings]
+    num_list = [input_setting['map_num']] + [setting['map_num'] for setting in network_settings]
+    loc_diff_each_map = [3, -3]
+    layer_width_padding = 50
+    layer_width_list = [max(size[-2:]) + NumConvMax * loc_diff_each_map[0] for size in size_list]
+
+    x_diff_list = [0] + [max(layer_width_min, width + layer_width_padding) for width in layer_width_list]
     text_list = ['Inputs'] + ['Feature\nmaps'] * (len(size_list) - 1)
-    loc_diff_list = [[3, -3]] * len(size_list)
+    loc_diff_list = [loc_diff_each_map] * len(size_list)
 
     num_show_list = map(min, num_list, [NumConvMax] * len(num_list))
     top_left_list = np.c_[np.cumsum(x_diff_list), np.zeros(len(x_diff_list))]
@@ -128,30 +148,34 @@ if __name__ == '__main__':
                   num=num_show_list[ind],
                   top_left=top_left_list[ind], loc_diff=loc_diff_list[ind])
         label(top_left_list[ind], text_list[ind] + '\n{}@{}x{}'.format(
-            num_list[ind], size_list[ind], size_list[ind]))
+            num_list[ind], size_list[ind][0], size_list[ind][1]))
 
 
     ############################
     # in between layers
-    start_ratio_list = [[0.4, 0.5], [0.4, 0.8], [0.4, 0.5], [0.4, 0.8]]
-    patch_size_list = [5, 2, 5, 2]
+    start_ratio_list = [[0.3, 0.4], [0.5, 0.8]] * ((len(network_settings) + 1)//2)
+    patch_size_list = [setting['kernel'] for setting in network_settings]
     ind_bgn_list = range(len(patch_size_list))
-    text_list = ['Convolution', 'Max-pooling', 'Convolution', 'Max-pooling']
+    text_list = [setting['type'] for setting in network_settings]
 
     for ind in range(len(patch_size_list)):
         add_mapping(patches, colors, start_ratio_list[ind],
                     patch_size_list[ind], ind,
                     top_left_list, loc_diff_list, num_show_list, size_list)
         label(top_left_list[ind], text_list[ind] + '\n{}x{} kernel'.format(
-            patch_size_list[ind], patch_size_list[ind]), xy_off=[26, -65])
+            patch_size_list[ind][0], patch_size_list[ind][1]), xy_off=[26, -layer_width_list[ind]-25])
 
 
     ############################
     # fully connected layers
-    size_list = [fc_unit_size, fc_unit_size, fc_unit_size]
-    num_list = [768, 500, 2]
+    network_settings = [{'type': 'Flatten\n',           'visual_size': fc_unit_size, 'num': 256},
+                        {'type': 'Dropout\n0.5',        'visual_size': fc_unit_size, 'num': 1024},
+                        {'type': 'Fully\nconnected',    'visual_size': fc_unit_size, 'num': 10}]
+    size_list = [[setting['visual_size']]*2 for setting in network_settings]
+    num_list = [setting['num'] for setting in network_settings]
+    layer_width_min = 60
     num_show_list = map(min, num_list, [NumFcMax] * len(num_list))
-    x_diff_list = [sum(x_diff_list) + layer_width, layer_width, layer_width]
+    x_diff_list = [sum(x_diff_list)] + [layer_width_min] * (len(size_list) -1)
     top_left_list = np.c_[np.cumsum(x_diff_list), np.zeros(len(x_diff_list))]
     loc_diff_list = [[fc_unit_size, -fc_unit_size]] * len(top_left_list)
     text_list = ['Hidden\nunits'] * (len(size_list) - 1) + ['Outputs']
@@ -162,7 +186,7 @@ if __name__ == '__main__':
         label(top_left_list[ind], text_list[ind] + '\n{}'.format(
             num_list[ind]))
 
-    text_list = ['Flatten\n', 'Fully\nconnected', 'Fully\nconnected']
+    text_list = [setting['type'] for setting in network_settings]
 
     for ind in range(len(size_list)):
         label(top_left_list[ind], text_list[ind], xy_off=[-10, -65])
@@ -175,10 +199,14 @@ if __name__ == '__main__':
     plt.tight_layout()
     plt.axis('equal')
     plt.axis('off')
-    plt.show()
-    fig.set_size_inches(8, 2.5)
+    # plt.show() # showing the plot will change the layout of the fig as the window changes
+
+    ############################
+    # output settings: tweak dpi and fig size for different network depth
+    Dpi = 150
+    fig.set_size_inches(20, 7.5)
 
     fig_dir = './'
     fig_ext = '.png'
     fig.savefig(os.path.join(fig_dir, 'convnet_fig' + fig_ext),
-                bbox_inches='tight', pad_inches=0)
+                bbox_inches='tight', pad_inches=0, dpi=Dpi)
